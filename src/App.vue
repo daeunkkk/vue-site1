6846283436f0bcb77fadf64729d67017855c97b3
sliverda <107448492+sliverda@users.noreply.github.com>
2022-07-17 12:01:28 +0900

    2022.7.17

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <HeaderSection />
+  <SliderSection />
+  <ImageSection skill="section" />
+  <ImgTextSection />
+</template>
+
+<script>
+import HeaderSection from "./components/HeaderSection.vue";
+import SliderSection from "./components/SliderSection.vue";
+import ImageSection from "./components/ImageSection.vue";
+import ImgTextSection from "./components/ImgTextSection.vue";
+export default {
+  name: "App",
+  components: {
+    HeaderSection,
+    SliderSection,
+    ImageSection,
+    ImgTextSection
+  },
+};
+</script>
+
+<style>
+@import "./App.css";
+</style>
